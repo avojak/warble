2dff0671141fe69fa0d6772455dd8589c520d64d
Andrew Vojak <andrew.vojak@gmail.com>
2022-01-12 18:12:33 -0700

    Fix possible answers not being a strict subset of the dictionary (#11)

---
+++ validate-words.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Tool to validate that the list of possible answers (words.txt) is a
+strict subset of the dictionary of acceptable words (dictionary.txt).
+
+This script should be run any time a change is made to either file.
+"""
+
+with open('data/words.txt') as words:
+    possible_answers = words.readlines()
+    possible_answers = [line.rstrip() for line in possible_answers]
+
+with open('data/dictionary.txt') as dictionary:
+    dictionary_words = dictionary.readlines()
+    dictionary_words = [line.rstrip() for line in dictionary_words]
+
+for possible_answer in possible_answers:
+    if possible_answer not in dictionary_words:
+        print(possible_answer, end='\n')
